af0d1ed472ad0f392f2783b8de35e7116981ae92
faizu <150577139+leenoera03@users.noreply.github.com>
2025-04-14 17:02:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
+
 app.use(
-    cors({
-      origin: "https://d-04-450714.uc.r.appspot.com/", 
-      methods: ["GET", "POST", "PATCH", "DELETE"],
-      credentials: true,
-    })
-  );
+  cors({
+    origin: "https://d-04-450714.uc.r.appspot.com/",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(UserRoute);
-// ini ni 
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Backend API is running" });
+});
+
+// Logger untuk semua request
 app.use((req, res, next) => {
   console.log(`Incoming request: ${req.method} ${req.url}`);
   next();
 });
 
+app.listen(5000, () => console.log("Server up and running...."));
 
-app.listen(5000, ()=> console.log('Server up and running....'));
